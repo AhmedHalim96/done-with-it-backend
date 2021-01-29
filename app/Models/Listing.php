c0b17fe67352e4eac8ccaced2326d4ac16a0c647
Ahmed Halim <ahmedhalim96@protonmail.com>
2021-01-29 08:34:21 +0200

    Adding idehelper package

--- app/Models/Listing.php
+++ app/Models/Listing.php
@@ -5,6 +5,26 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * App\Models\Listing
+ *
+ * @property int $id
+ * @property string $title
+ * @property string|null $description
+ * @property string $photos
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @method static \Illuminate\Database\Eloquent\Builder|Listing newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Listing newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Listing query()
+ * @method static \Illuminate\Database\Eloquent\Builder|Listing whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Listing whereDescription($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Listing whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Listing wherePhotos($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Listing whereTitle($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Listing whereUpdatedAt($value)
+ * @mixin \Eloquent
+ */
 class Listing extends Model
 {
     use HasFactory;
