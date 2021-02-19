660936176c490e5741ab4d4206dc3362618a3f29
Ahmed Halim <ahmedhalim96@protonmail.com>
2021-02-19 15:53:18 +0200

    seeding database with categories

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Category;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,5 +15,18 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+
+        $furniture = new Category();
+        $furniture->title = "Furniture";
+        $furniture->save();
+
+        $clothing = new Category();
+        $clothing->title = "Clothing";
+        $clothing->save();
+
+        $technology = new Category();
+        $technology->title = "Technology";
+        $technology->save();
+
     }
 }
