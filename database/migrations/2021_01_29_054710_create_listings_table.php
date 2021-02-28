3e5c79156541b4e979d3d470a585ddc0fa0d8510
Ahmed Halim <ahmedhalim96@protonmail.com>
2021-02-28 14:00:17 +0200

    Adding Missing Price column

--- database/migrations/2021_01_29_054710_create_listings_table.php
+++ database/migrations/2021_01_29_054710_create_listings_table.php
@@ -16,6 +16,7 @@ class CreateListingsTable extends Migration
         Schema::create('listings', function (Blueprint $table) {
             $table->id();
             $table->string("title", 55);
+            $table->integer("price");
             $table->text("description")->nullable();
             $table->text("photo");
             $table->foreignId("category_id")
