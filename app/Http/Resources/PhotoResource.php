7541b1cc6e55e83dba9741bd0e77d694b422ebb3
Ahmed Halim <ahmedhalim96@protonmail.com>
2021-03-13 16:02:43 +0200

    add id field to photo resources

--- app/Http/Resources/PhotoResource.php
+++ app/Http/Resources/PhotoResource.php
@@ -15,6 +15,7 @@ class PhotoResource extends JsonResource
     public function toArray($request)
     {
         return [
+            "id" => $this->id,
             "url" => $this->url,
         ];
     }
