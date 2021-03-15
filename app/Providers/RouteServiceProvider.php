12c268ae7047dc447bd8e859373ceb65b6c9baf7
Ahmed Halim <ahmedhalim96@protonmail.com>
2021-03-15 14:23:50 +0200

    fixing namespace

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -40,7 +40,7 @@ class RouteServiceProvider extends ServiceProvider
         $this->routes(function () {
             Route::prefix('api/v1')
                 ->middleware('api')
-                ->namespace($this->namespace . "/V1")
+                ->namespace($this->namespace)
                 ->group(base_path('routes/api.php'));
 
             Route::middleware('web')
