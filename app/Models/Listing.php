4e9e35acddc67bb5acc8953529d30f7c1a61826a
Ahmed Halim <ahmedhalim96@protonmail.com>
2021-06-02 08:44:00 +0200

    adding user listing relations

--- app/Models/Listing.php
+++ app/Models/Listing.php
@@ -38,10 +38,16 @@ class Listing extends Model
     use HasFactory;
     protected $fillable = ["title", "price", "description", "category_id"];
 
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
     public function category()
     {
         return $this->belongsTo(Category::class);
     }
+
     public function photos()
     {
         return $this->hasMany(Photo::class);
